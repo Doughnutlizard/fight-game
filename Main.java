dac4b55d66ea2646fb8d9bc3f8fa20f9fcc9e1c0
Doughnutlizard <dancaxambu@outlook.com>
2026-01-14 16:48:38 -0800

    Made some simple base code

--- Main.java
+++ Main.java
@@ -1,3 +1,29 @@
-public class Main {
+import java.util.*;
 
+public class Main {
+    public static void main(String[] args) {
+        startGame();
+    }
+
+    public static void startGame() {
+        Scanner userIn = new Scanner (System.in);
+        System.out.println("What would you like to do?");
+        System.out.println("[1]-Fight [2]-View Characters [3]-Create New [4]-Save Creation");
+        int num = userIn.nextInt();
+        if (num == 1) {
+            fight();
+        } else if (num == 2) {
+            viewChars();
+        } else if (num == 3) {
+            createNewChar();
+        } else {
+            saveChar();
+        }
+        userIn.close();
+    }
+
+    public static void fight() {}
+    public static void viewChars() {}
+    public static void createNewChar() {}
+    public static void saveChar() {}
 }
